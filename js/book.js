9ffdc6cda5701d894960bafe5ae20e101aef2e58
Brent Smith <brent.smith@techtonic.com>
2018-10-11 12:39:06 -0600

    Finish all required functions

--- js/book.js
+++ js/book.js
@@ -4,9 +4,21 @@ function Book(title, author, numPages, pubDate)
   this.author = author;
   this.numPages = numPages;
   this.pubDate = pubDate; //date object
-};
+}
 
-Book.prototype.editBook = function(oBook)
+Book.prototype.editBook = function (oBook)
 {
+  if (oBook.title !== undefined) {
+    this.title = oBook.title;
+  }
 
+  if (oBook.author !== undefined) {
+    this.author = oBook.author;
+  }
+
+  if (oBook.numPages !== undefined) {
+    this.numPages = oBook.numPages;
+  }
+
+  return this;
 };
--- js/engine.js
+++ js/engine.js
@@ -1,15 +1,13 @@
-function Library()
-{
+function Library() {
   this.bookShelf = new Array();
-};
+}
 
-Library.prototype.addBook = function(book)
-{
-  //check if book is already in my array (this.bookShelf)
+Library.prototype.addBook = function (book) {
+  //Check if book is already in my array (this.bookShelf)
   //Add book thats coming from book param if the title is not already in arr
   //return true if book was added and false if it was not add or already exists
-  for(var i=0; i<this.bookShelf.length; i++) {
-    if(this.bookShelf[i].title === book.title){
+  for (var i = 0; i < this.bookShelf.length; i++) {
+    if (this.bookShelf[i].title === book.title) {
       return false;
     }
   }
@@ -18,63 +16,92 @@ Library.prototype.addBook = function(book)
   return true;
 };
 
-Library.prototype.removeBookByTitle = function(title)
-{
-  for(var i=0; i<this.bookShelf.length; i++) {
-    if(this.bookShelf[i].title === title){
-      this.bookShelf.splice(i, 1);{
+Library.prototype.removeBookByTitle = function (title) {
+  for (var i = 0; i < this.bookShelf.length; i++) {
+    if (this.bookShelf[i].title === title) {
+      this.bookShelf.splice(i, 1); {
         return true;
       }
     }
   }
+
   return false;
 };
 
-Library.prototype.removeBookByAuthor = function(author)
-  {
-    var haveRemoved = false;
-    for(var i=0; i<this.bookShelf.length; i++) {
-      if(this.bookShelf[i].author === author){
-        this.bookShelf.splice(i,1);
-        i--;
-        haveRemoved = true;
-      }
+Library.prototype.removeBookByAuthor = function (authorName) {
+  var haveRemoved = false;
+  for (var i = 0; i < this.bookShelf.length; i++) {
+    if (this.bookShelf[i].author === authorName) {
+      this.bookShelf.splice(i, 1);
+      i--;
+      haveRemoved = true;
     }
-    return haveRemoved;
+  }
+
+  return haveRemoved;
 };
 
-Library.prototype.getRandomBook = function()
-{
+Library.prototype.getRandomBook = function () {
   if (this.bookShelf.length) {
     var i = this.bookShelf[Math.floor(Math.random() * this.bookShelf.length)];
-      return i;
-    }
-  else {
-      return null;
+    return i;
+  } else {
+    return null;
   }
 };
 
-Library.prototype.getBookByTitle = function(title)
-{
-  var matchingTitles = this.bookShelf.filter(item => item.title.indexOf(title) > -1);
-  return (matchingTitles);
+Library.prototype.getBookByTitle = function (title) {
+  var matchingTitles = this.bookShelf.filter(function (book) {
+    return book.title.indexOf(title) > -1;
+  });
+
+  return matchingTitles;
 };
-// Sonali helped with this one. Need to get ride of arrow function
 
-// Library.prototype.getBookByTitle = function(title)
-// {
-//   var matchingTitles = this.bookShelf.filter(this.title.indexOf(title) > -1);
-//   return (matchingTitles);
-// };
+Library.prototype.getBookByAuthor = function (authorName) {
+  var matchingAuthors = this.bookShelf.filter(function (book) {
+    return book.author.indexOf(authorName) > -1;
+  });
 
-document.addEventListener("DOMContentLoaded", function(e){
+  return matchingAuthors;
+};
+
+Library.prototype.addBooks = function (books) {
+    var start = this.bookShelf.length;
+    for (var i = 0; i < books.length; i++) {
+      this.addBook(books[i]);
+    }
+
+    var after = this.bookShelf.length;
+    return (after - start);
+  };
+
+Library.prototype.getAuthors = function () {
+    var array = [];
+    for (var i = 0; i < this.bookShelf.length; i++) {
+      var authors = this.bookShelf[i].author;
+      array.push(authors);
+    }
+
+    return array;
+  };
+
+Library.prototype.getRandomAuthorName = function () {
+    var random = this.getRandomBook();
+    return random.author;
+  };
+
+document.addEventListener('DOMContentLoaded', function (e) {
   window.gLibrary = new Library();
 
-  window.book1 = new Book("It", "Stephen King", 200, "Jan 1 2000");
-  window.book2 = new Book("The Shining", "Stephen King", 201, "Feb 2 2001");
-  window.book3 = new Book("Green Eggs and Ham", "Dr Seuss", 202, "Mar 3 2002");
-  window.book4 = new Book("Harry Potter", "JK Rowling", 203, "Apr 4 2003");
-  window.book5 = new Book("Jaws", "Peter Benchley", 204, "May 5 2004");
+  window.book1 = new Book('It', 'Stephen King', 200, 'Jan 1 2000');
+  window.book2 = new Book('The Shining', 'Stephen King', 201, 'Feb 2 2001');
+  window.book3 = new Book('Green Eggs and Ham', 'Dr Seuss', 202, 'Mar 3 2002');
+  window.book4 = new Book('Harry Potter', 'JK Rowling', 203, 'Apr 4 2003');
+  window.book5 = new Book('Jaws', 'Peter Benchley', 204, 'May 5 2004');
+
+  //test book for testing edit book function. Also works if just passing an Object in the console
+  window.oBook1 = { title: 'Catch 22', numPages: 305 };
 
   gLibrary.addBook(book1);
   gLibrary.addBook(book2);
@@ -82,7 +109,3 @@ document.addEventListener("DOMContentLoaded", function(e){
   gLibrary.addBook(book4);
   gLibrary.addBook(book5);
 });
-
-
-//how is add book function being attached tured into a book object vs just an object
-//
